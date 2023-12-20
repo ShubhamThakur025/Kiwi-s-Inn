bf27730d940f7211939fda2349b612015dc11e25
ShubhamThakur025 <thakur0805@gmail.com>
2023-12-20 10:21:00 +0530

    Search functionality added

--- script.js
+++ script.js
@@ -4,6 +4,11 @@ let pageRightIng = document.querySelector('.page-right-ing')
 let instrcutionList = document.getElementById('ingredient-list')
 let recipeList = document.getElementById('recipe-list')
 
+let searchBox = document.querySelector('.search')
+let searchBtn = document.getElementById('search-click')
+
+let dishesTable = document.querySelector('.dishes-table')
+let table = document.getElementById('table')
 fetch('https://www.themealdb.com/api/json/v1/1/random.php')
     .then(response => {
         console.log(response)
@@ -12,27 +17,71 @@ fetch('https://www.themealdb.com/api/json/v1/1/random.php')
     .then(result => {
         console.log(result.meals[0])
         randomImage.setAttribute('src', result.meals[0]['strMealThumb'])
-        pageRightIng.innerHTML =
-            `
-    <img src="${result.meals[0]['strMealThumb']}" alt="" class="meal-img">
-    <p class = "desc" >${result.meals[0]['strMeal']}</p>    
-    `
-        pageRightRecipe.innerHTML =
-            `
-    <img src="${result.meals[0]['strMealThumb']}" alt="" class="meal-img">
-    <p class = "desc">${result.meals[0]['strMeal']}</p>    
-    `
-        for (let i = 1; i <= 20; i++) {
-            if (result.meals[0][`strIngredient${i}`] != '') {
-                instrcutionList.innerHTML +=
-                    `<li>${result.meals[0][`strIngredient${i}`]} </li>`
+        updateIngredients(result)
+        updateRecipe(result)
 
-            }
-            else {
-                break;
-            }
-        }
-        recipeList.innerHTML = result.meals[0][`strInstructions`]
 
     })
 
+let searchResults = (searchItem) => {
+    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${searchItem}`)
+        .then(response => response.json())
+        .then((result) => {            
+            serveOnTheTable(result.meals)
+        })
+}
+
+
+let updateIngredients = (result) => {
+    for (let i = 1; i <= 20; i++) {
+        if (result.meals[0][`strIngredient${i}`] != '') {
+            instrcutionList.innerHTML +=
+                `<li>${result.meals[0][`strIngredient${i}`]} </li>`
+
+        }
+        else {
+            break;
+        }
+    }
+    pageRightIng.innerHTML =
+        `
+    <img src="${result.meals[0]['strMealThumb']}" alt="" class="meal-img">
+    <p class = "desc" >${result.meals[0]['strMeal']}</p>    
+    `
+}
+
+let updateRecipe = (result) => {
+    pageRightRecipe.innerHTML =
+        `
+    <img src="${result.meals[0]['strMealThumb']}" alt="" class="meal-img">
+    <p class = "desc">${result.meals[0]['strMeal']}</p>    
+    `
+
+    recipeList.innerHTML = result.meals[0][`strInstructions`]
+}
+
+let serveOnTheTable = (dishes) => {
+    dishesTable.style.display = 'block'
+    dishes.forEach(dish => {
+        console.log(dish)
+        table.innerHTML +=
+            `
+        <div class="dish">
+            <img src="${dish.strMealThumb}" alt="" >
+            <p class = "food-desc">${dish.strMeal}</p>
+        </div>        
+        `
+        dishesTable.scrollIntoView({behavior: 'smooth'})
+    });
+}
+
+searchBtn.onclick = () => {
+    if (searchBox.value) {
+        console.log(searchBox.value)
+        searchResults(searchBox.value)
+    }
+    else {
+        alert("Enter a valid search value")
+    }
+
+}
